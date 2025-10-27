ad274daafa8b843e70b5b2b0f7d76fba19b73e56
Carlos <carlosfvargas526@gmail.com>
2025-10-27 02:35:18 -0700

    cleaned main file

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of AnagramLookupTable
         AnagramSetBuilder setBuilder = new AnagramSetBuilder();
-        
-        String word = "blacks";
-        // int hash = table.computeUnorderedHash(word);
-        // System.out.println("\nHash value for \'" + word + "\': " + hash + "\n");
         
         // setBuilder.buildSets("words.txt");
         // setBuilder.buildSets("words");
