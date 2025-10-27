33744446f7a382e351096ef01e4422c1a329a4d8
Carlos <carlosfvargas526@gmail.com>
2025-10-27 02:37:24 -0700

    cleaned main file

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         AnagramSetBuilder setBuilder = new AnagramSetBuilder();
         
-        // setBuilder.buildSets("words.txt");
-        // setBuilder.buildSets("words");
-        // setBuilder.buildSets("wordswords");
+        // setBuilder.buildSets("words.txt"); // class file - 100k words
+        // setBuilder.buildSets("words"); // /usr/share/dict/words - 250k words
+        // setBuilder.buildSets("wordswords"); // /usr/share/dict/words multiplied - 10 million words
         setBuilder.buildSets("words2.txt");
         setBuilder.printNumberOfSets(true);
         setBuilder.printSets(false);
